2db7f3923db32853ca4eeb6a833b8b71756fb625
sunset-summer <alex1123321@gmail.com>
2021-03-15 06:35:18 +0200

    fead: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let numbString = Array.from(str);
+    if (numbString[0] == "-") {
+        numbString.splice(0,1);
+    }
+    let reversedArr = numbString.reverse();
+    let reversedStr = reversedArr.join('');
+    return reversedStr;
 }
